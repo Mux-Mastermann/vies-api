c0a16119f926735c66d167283f289d6d9ae3b9ee
Mux-Mastermann <jan@knorr.family>
2022-03-24 20:05:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+STATES = {
+    "AT": "Austria",
+    "BE": "Belgium",
+    "BG": "Bulgaria",
+    "CY": "Cyprus",
+    "CZ": "Czech Republic",
+    "DE": "Germany",
+    "DK": "Denmark"
+}
+
+def check_vat_id(state, vat_number):
+    pass
